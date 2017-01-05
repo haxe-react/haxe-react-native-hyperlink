91641da10aa067b4d2d476fb7656fb971fcd3a19
Kevin Leung <kevinresol@gmail.com>
2017-01-05 11:54:08 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# haxe-react-native-hyperlink
+
+Haxe Extern for react-native-hyperlink
---
+++ src/react/native/hyperlink/Hyperlink.hx
@@ -0,0 +1,4 @@
+package react.native.hyperlink;
+
+@:jsRequire('react-native-hyperlink')
+extern class Hyperlink extends react.ReactComponent {}
